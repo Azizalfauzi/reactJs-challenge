e9f1c7afc2e6ed5c4be4b83690ffc7b6382dcebc
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-01 14:40:26 +0700

    Menambahkan css dan html layout

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <h1>Avatar</h1>;
+}
+
+function Intro() {
+  return <h1>Title</h1>;
+}
+
+function SkillList() {
+  return <h1>Kist of Icon</h1>;
 }
